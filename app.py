ff03ed8a99fbd7cf16ce192f880c872cd7da2734
Henry Orsborn <henry@harrspc.com>
2024-12-15 14:32:31 -0600

    Added auth

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
 from flask import Flask
-import os
+from sqlalchemy import event
+import os, boto3
 import sqlalchemy
+
 app = Flask("JimminyCricket")
 
-engine = sqlalchemy.create_engine(f"mysql://{os.environ.get('DB_USER')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}")
+session = boto3.Session(profile_name='default')
+client = session.client('rds')
+
+dbuser = os.environ.get('DB_USER')
+dbhost = os.environ.get('DB_HOST')
+dbname = os.environ.get('DB_NAME')
+dbport = os.environ.get('DB_PORT')
+region = os.environ.get('REGION')
+
+engine = sqlalchemy.create_engine(f"mysql://{dbuser}@{dbhost}/{dbname}")
 db = engine.connect()
+
+
+@event.listens_for(engine, "do_connect")
+def provide_token(dialect, conn_rec, cargs, cparams):
+    cparams["token"] = get_db_token()
+
+
+def get_db_token():
+    return client.generate_db_auth_token(
+        DBHostname=dbhost,
+        Port=dbport,
+        DBUsername=dbuser,
+        Region=region)
+
 
 @app.route("/")
 def health():
     return f"TableNames: {db.execute('SHOW TABLES').fetchall()}"
+
 
 if __name__ == "__main__":
     app.run()
--- scripts/setup_application.sh
+++ scripts/setup_application.sh
@@ -7,6 +7,6 @@ sudo a2ensite jimminycricket.conf
 sudo apt update && sudo apt install python3.12-venv python3-pip apache2 -y
 sudo apt-get install python3-dev libmysqlclient-dev -y
 pip3 install virtualenv
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv /srv/jimminycricket2/venv
+source /srv/jimminycricket2/venv/bin/activate
 pip3 install -r /srv/jimminycricket2/requirements.txt
