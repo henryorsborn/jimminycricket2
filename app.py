39877630301e5bb78a7889bb2b73efa1405e3756
Henry Orsborn <henry@harrspc.com>
2024-12-15 11:32:50 -0600

    print test env variable

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def health():
-    return "Hello World."
+    return f"Hello World. Here's the environment variable {os.environ.get('TEST_ENV_VAR')}"
 
 if __name__ == "__main__":
     app.run()
