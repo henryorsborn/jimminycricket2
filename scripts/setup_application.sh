16a0eb31327532f3e376d8816d65ecaadb2ce312
Henry Orsborn <henry@harrspc.com>
2024-12-14 21:51:09 -0600

    Removed sudo

--- scripts/setup_application.sh
+++ scripts/setup_application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd srv/jimminycricket2
 sudo apt update && sudo apt install python3.12-venv python3-pip apache2
-sudo pip3 install virtualenv
+pip3 install virtualenv
 virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
