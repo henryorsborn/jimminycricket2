a13c1782a034d353da2bef1605dae01cb93abd61
Henry Orsborn <henry@harrspc.com>
2024-12-14 21:57:37 -0600

    change to venv

--- scripts/setup_application.sh
+++ scripts/setup_application.sh
@@ -2,6 +2,6 @@
 cd srv/jimminycricket2
 sudo apt update && sudo apt install python3.12-venv python3-pip apache2
 pip3 install virtualenv
-virtualenv venv
+python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
