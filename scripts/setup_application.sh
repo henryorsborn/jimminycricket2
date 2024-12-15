c21e1a0a03073eee36136b396dea79ba53720b21
Ubuntu <ubuntu@ip-172-31-29-206.ap-southeast-1.compute.internal>
2024-12-15 03:22:27 +0000

    added git ignore and removed mistakenly added sudo

--- scripts/setup_application.sh
+++ scripts/setup_application.sh
@@ -3,4 +3,4 @@ sudo apt update && sudo apt install python3.12-venv python3-pip apache2
 sudo pip3 install virtualenv
 virtualenv venv
 source venv/bin/activate
-sudo pip3 install -r requirements.txt
+pip3 install -r requirements.txt
