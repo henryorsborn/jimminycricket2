2520fcf46778e3fa251e67ba0ad291e3da38be03
Henry Orsborn <henry@harrspc.com>
2024-12-15 13:31:51 -0600

    Adding dependency

--- scripts/setup_application.sh
+++ scripts/setup_application.sh
@@ -4,8 +4,8 @@ sudo cp /srv/jimminycricket2/config/jimminycricket.service /etc/systemd/system/
 sudo cp /srv/jimminycricket2/config/jimminycricket.conf /etc/apache2/sites-available/
 sudo a2dissite *.conf
 sudo a2ensite jimminycricket.conf
-sudo apt update && sudo apt install python3.12-venv python3-pip apache2
-sudo apt-get install python3-dev libmysqlclient-dev
+sudo apt update && sudo apt install python3.12-venv python3-pip apache2 -y
+sudo apt-get install python3-dev libmysqlclient-dev -y
 pip3 install virtualenv
 python3 -m venv venv
 source venv/bin/activate
