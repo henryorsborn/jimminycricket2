19381218182421eb4d07233f8c67932cc6e8026d
Henry Orsborn <henry@harrspc.com>
2024-12-15 12:58:47 -0600

    fixed typos

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 import os
 app = Flask("JimminyCricket")
 
-engine = sqlalchemy.create_engine(f"mysql://{os.environ.get('DB_USER'}@{os.environ.get('DB_HOST'}/{os.environ.get('DB_NAME'}")
+engine = sqlalchemy.create_engine(f"mysql://{os.environ.get('DB_USER')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}")
 db = engine.connect()
 
 @app.route("/")
