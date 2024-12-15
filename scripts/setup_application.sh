ee6a8485df1b253673d00a7d6a92df6075d370cf
Henry Orsborn <henry@harrspc.com>
2024-12-14 21:59:03 -0600

    Update path for requirements.txt

--- scripts/setup_application.sh
+++ scripts/setup_application.sh
@@ -4,4 +4,4 @@ sudo apt update && sudo apt install python3.12-venv python3-pip apache2
 pip3 install virtualenv
 python3 -m venv venv
 source venv/bin/activate
-pip3 install -r requirements.txt
+pip3 install -r /srv/jimminycricket/requirements.txt
