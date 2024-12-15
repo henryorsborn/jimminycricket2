9d3367bc7ad0c2a3d8b91e8334b1d86b0305b31a
Henry Orsborn <henry@harrspc.com>
2024-12-15 13:05:25 -0600

    Add missing imports

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import os
+import sqlalchemy
 app = Flask("JimminyCricket")
 
 engine = sqlalchemy.create_engine(f"mysql://{os.environ.get('DB_USER')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}")
